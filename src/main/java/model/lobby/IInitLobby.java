fb063c5a48afa8f48993ce8175ecfea2df53665b
acx593 <dominik.martin@haw-hamburg.de>
2022-12-20 17:52:13 +0100

    added Lobby methods

--- src/main/java/model/lobby/IInitLobby.java
+++ src/main/java/model/lobby/IInitLobby.java
@@ -1,4 +1,10 @@
 package model.lobby;
 
+import model.player.Player;
+
 public interface IInitLobby {
+
+    public Lobby initLobby();
+
+    public void plateJoin(Player player);
 }
--- src/main/java/model/lobby/Lobby.java
+++ src/main/java/model/lobby/Lobby.java
@@ -1,4 +1,25 @@
 package model.lobby;
 
+import model.player.Player;
+
 public class Lobby implements ILobbyGameLogic, IInitLobby{
+    @Override
+    public void endGame() {
+
+    }
+
+    @Override
+    public void createWinnerScreen() {
+
+    }
+
+    @Override
+    public Lobby initLobby() {
+        return null;
+    }
+
+    @Override
+    public void plateJoin(Player player) {
+
+    }
 }
