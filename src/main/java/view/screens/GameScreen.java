1fb7e921270654eb99552ba923b0fb2cbf11f4c6
acx593 <dominik.martin@haw-hamburg.de>
2022-12-20 17:31:37 +0100

    deleted getColor method

--- src/main/java/view/screens/GameScreen.java
+++ src/main/java/view/screens/GameScreen.java
@@ -22,7 +22,7 @@ public class GameScreen extends VBox implements Screen{
     public void drawBikes(Map<Integer, int[][]> bikePos, ITronView view){
         for (Map.Entry<Integer, int[][]> entry : bikePos.entrySet()){
             int colorNum = entry.getKey();
-            String color = getColor(colorNum);
+            String color = Enums.Color.values()[colorNum].toString();
             Color bikeColor = Color.valueOf(color);
             List<Coordinate> coordinates = new ArrayList<>();
             for(int i = 0; i < entry.getValue().length;){
@@ -34,38 +34,4 @@ public class GameScreen extends VBox implements Screen{
             view.draw(coordinates, bikeColor);
         }
     }
-
-    public String getColor(int colorNum){
-        String color = "";
-
-        switch(colorNum){
-            case 0:
-                color = "grey";
-                break;
-            case 1:
-                color = "red";
-                break;
-            case 2:
-                color = "blue";
-                break;
-            case 3:
-                color = "yellow";
-                break;
-            case 4:
-                color = "green";
-                break;
-            case 5:
-                color = "brown";
-                break;
-            case 6:
-                color = "purple";
-                break;
-
-        }
-
-        return color;
-
-
-    }
-
 }
