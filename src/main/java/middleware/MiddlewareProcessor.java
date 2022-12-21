44cf4168a842edfe20fd8071e3c06d679e7ec130
acq831 <dominik.mueller@haw-hamburg.de>
2022-12-21 02:56:06 +0100

    Add first pieces of middleware generator code

---
+++ src/main/java/middleware/MiddlewareProcessor.java
@@ -0,0 +1,42 @@
+package middleware;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+@SupportedAnnotationTypes("middleware.annotation.RemoteMethod")
+@SupportedSourceVersion(SourceVersion.RELEASE_17)
+public class MiddlewareProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for(TypeElement annotation : annotations) {
+            Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
+            Map<Boolean, List<Element>> annotatedMethods = annotatedElements.stream().collect(
+                Collectors.partitioningBy(element -> 
+                    element.getEnclosingElement().getKind().isInterface()
+                )
+            );
+
+            List<Element> interfaceMethods = annotatedMethods.get(true);
+            List<Element> otherMethods = annotatedMethods.get(false);
+            otherMethods.forEach(element -> 
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "RemoteMethod may only be applied to a method inside an interface", element)
+            );
+
+            
+        }
+        return false;
+    }
+    
+}
