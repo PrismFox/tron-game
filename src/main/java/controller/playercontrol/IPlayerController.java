bb19a3d615fe578488342e634d821a1693606ac7
Dominik M <muellerxdominik@gmail.com>
2022-12-23 03:39:59 +0100

    Remove test annotation

--- src/main/java/controller/playercontrol/IPlayerController.java
+++ src/main/java/controller/playercontrol/IPlayerController.java
@@ -2,9 +2,6 @@ package controller.playercontrol;
 
 import java.util.List;
 
-import middleware.annotation.RemoteInterface;
-
-@RemoteInterface
 public interface IPlayerController {
     public void onKeyPress(String key);
 
