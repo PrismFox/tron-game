ce6efb6243ff864ce8db6f46b6baf586af0854d3
acu704 <can.heintze@haw-hamburg.de>
2022-12-24 16:03:57 +0100

    #4 [ADD] getter and winnerstatus

--- src/main/java/model/gameLogic/IPlayerLogic.java
+++ src/main/java/model/gameLogic/IPlayerLogic.java
@@ -1,6 +1,16 @@
 package model.gameLogic;
 
+import model.player.IPlayerManager;
+
 public interface IPlayerLogic {
     void killPlayer(int playerId);
+
+    /**
+     * @return A 2d array. First index represents the winning status, 0 = draw, 1 = win. The second index stands for
+     * the player. If it's a draw the player value will be -1, otherwise it's the player id
+     */
+    int[] getWinnerStatus();
+
+    IPlayerManager getPlayerManager();
 
 }
--- src/main/java/model/gameLogic/PlayerLogic.java
+++ src/main/java/model/gameLogic/PlayerLogic.java
@@ -1,22 +1,26 @@
 package model.gameLogic;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import model.player.IPlayerManager;
+import model.player.Player;
 import model.player.PlayerManager;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Slf4j
 @Data
-@Service
+@Component
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class PlayerLogic implements IPlayerLogic {
 
-    private PlayerManager playerManager;
+    private final IPlayerManager playerManager;
 
-    public PlayerLogic(){
-        this.playerManager = new PlayerManager();
-    }
+
 
     @Override
     public void killPlayer(int playerId) {
@@ -25,6 +29,24 @@ public class PlayerLogic implements IPlayerLogic {
 
     public IPlayerManager getPlayerManager() {
         return playerManager;
+    }
 
+    @Override
+    public int[] getWinnerStatus() {
+        List<Player> livingPlayers = getPlayerManager().getLivingPlayers();
+        int[] result = new int[2];
+
+        if (livingPlayers.size() == 0) {
+
+            result[1] = -1;
+
+        }
+
+        if (livingPlayers.size() == 1) {
+            result[0] = 1;
+            result[1] = livingPlayers.get(0).getId();
+        }
+
+        return result;
     }
 }
