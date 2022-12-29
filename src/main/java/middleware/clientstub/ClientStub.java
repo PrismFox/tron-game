0ff6eedf6fb67383ed176ce1e046c7aaecdf04c9
acu704 <can.heintze@haw-hamburg.de>
2022-12-29 14:18:34 +0100

    #6 [ADD] send and receive method for udp and the nameserver lookup method

--- src/main/java/middleware/clientstub/ClientStub.java
+++ src/main/java/middleware/clientstub/ClientStub.java
@@ -1,14 +1,13 @@
 package middleware.clientstub;
 
 import Enums.TransportType;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import middleware.marshaler.INameServerMarshaler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.net.DatagramSocket;
-import java.net.Socket;
-import java.net.SocketException;
+import java.net.*;
 import java.util.List;
 
 @Slf4j
@@ -17,11 +16,16 @@ public class ClientStub implements IClientStub{
 
     String nameServerIp = "192.0.0.1";
     int port = 5549;
+    public final static int UDP_PACKET_SIZE = 1024;
 
     private DatagramSocket udpSocket;
     private Socket tcpSocket;
+    private INameServerMarshaler nameServerMarshaler;
+    private DatagramPacket udpReceivePacket;
 
-    public ClientStub() throws SocketException {
+    @Autowired
+    public ClientStub(INameServerMarshaler nameServerMarshaler) throws SocketException {
+        this.nameServerMarshaler = nameServerMarshaler;
         udpSocket = new DatagramSocket();
     }
 
@@ -44,8 +48,42 @@ public class ClientStub implements IClientStub{
         }
     }
 
-    private List<String> lookUp(){
-        return null;
+    private List<String> lookUp(String methodName) throws UnknownHostException {
+        String queryJson = nameServerMarshaler.marshalQueryRequest(methodName);
+
+        byte[] queryJsonBytes = queryJson.getBytes();
+        InetAddress ip = InetAddress.getByName(nameServerIp);
+        sendUDPPacket(queryJsonBytes, queryJsonBytes.length, ip, port);
+
+        String reponseString = receiveUDPPacket();
+
+        return nameServerMarshaler.unmarshal(reponseString);
+
+    }
+
+    private void sendUDPPacket(byte[] content, int length, InetAddress ipAddress, int port) {
+        DatagramPacket packet = new DatagramPacket(content, length, ipAddress, port);
+        try {
+            udpSocket.send(packet);
+        } catch (IOException e) {
+            log.error(String.format("Packet couldn't be sent to address: %s with port: %d", ipAddress.getHostAddress(), port));
+            e.printStackTrace();
+        }
+    }
+
+    private String receiveUDPPacket(){
+        String response = null;
+        try {
+            byte[] receivedData = new byte[UDP_PACKET_SIZE];
+            udpReceivePacket = new DatagramPacket(receivedData, UDP_PACKET_SIZE);
+            udpSocket.receive(udpReceivePacket);
+            response =  new String(udpReceivePacket.getData(), 0, udpReceivePacket.getLength());
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+       return response;
     }
 
 
