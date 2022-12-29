c8df096ef5576dbab4166df01804f8eb32aed3e5
acu704 <can.heintze@haw-hamburg.de>
2022-12-29 14:32:10 +0100

    #6 [ADD] classType paramter to invokeAsynchron

--- src/main/java/middleware/clientstub/IClientStub.java
+++ src/main/java/middleware/clientstub/IClientStub.java
@@ -2,9 +2,11 @@ package middleware.clientstub;
 
 import Enums.TransportType;
 
+import java.net.UnknownHostException;
+
 public interface IClientStub {
     
     Object invokeSynchronously(String methodName, Class<?> returnType, Object... args);
 
-    void invokeAsynchron(String methodName, TransportType transportType, Object... args);
+    void invokeAsynchron(String methodName, TransportType transportType, Class<?> classType, Object... args) throws UnknownHostException;
 }
