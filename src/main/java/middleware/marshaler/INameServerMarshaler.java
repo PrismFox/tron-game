1eb22d917cf99a2f48c918a6414146eac7609013
acu704 <can.heintze@haw-hamburg.de>
2022-12-29 14:13:52 +0100

    #6 [ADD] unmarshal and mashalNameServerRequest implementation

--- src/main/java/middleware/marshaler/INameServerMarshaler.java
+++ src/main/java/middleware/marshaler/INameServerMarshaler.java
@@ -1,9 +1,13 @@
 package middleware.marshaler;
 
+import java.util.List;
+
 public interface INameServerMarshaler {
 
     String marshalQueryRequest(String methodName);
 
     String marshalRegisterRequest(String methodName);
+
+    List<String> unmarshal(String responseJson);
 
 }
--- src/main/java/middleware/marshaler/NameServerMarshaler.java
+++ src/main/java/middleware/marshaler/NameServerMarshaler.java
@@ -1,6 +1,10 @@
 package middleware.marshaler;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class NameServerMarshaler implements INameServerMarshaler{
@@ -15,8 +19,23 @@ public class NameServerMarshaler implements INameServerMarshaler{
         return this.marshalNameServerRequest("register", methodName);
     }
 
+    @Override
+    public List<String> unmarshal(String responseJson){
+        JSONObject response = new JSONObject(responseJson);
+        List<String> address = new ArrayList<>();
+
+        address.add(response.getString("ip"));
+        address.add(response.getString("port"));
+
+        return address;
+    }
+
     private String marshalNameServerRequest(String methodType,String methodName){
-        return null;
+        JSONObject request = new JSONObject();
+        request.put("methodType", methodType);
+        request.put("methodName", methodName);
+
+        return request.toString();
     }
 
 
