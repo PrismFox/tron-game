0a30323330902f7cb12f0a4ecdef1da827bd51ac
Dominik M <muellerxdominik@gmail.com>
2023-01-05 15:59:40 +0100

    Add unmarshaling

--- src/main/java/de/haw/vsp/tron/middleware/marshaler/Marshaler.java
+++ src/main/java/de/haw/vsp/tron/middleware/marshaler/Marshaler.java
@@ -31,28 +31,67 @@ public class Marshaler implements IMarshaler {
     @Override
     public ResponseObject unmarshalClientStub(String message) {
         JSONObject json = new JSONObject(message);
-        Object response = null;
+        Object returnObject = null;
 
-        String responseType = json.getString("return_type");
-        try {
-            Class<?> responseClass = Class.forName(responseType);
-            Constructor<?> responseConstructor = responseClass.getConstructor();
-
+        String returnType = json.getString("return_type");
+        if (!returnType.contains("[]")) {
             String returnValue = json.getString("return_value");
-            response = responseConstructor.newInstance(returnValue);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            returnObject = unmarshalObject(returnType, returnValue);
+        }else{
+        JSONArray returnArray = json.getJSONArray("return_value");
+            returnObject = unmarshalArray(returnType.replaceFirst("\\[]", ""), returnArray);
         }
 
         long messageId = json.getLong("msg_id");
-        return new ResponseObject(messageId, response);
+        return new ResponseObject(messageId,returnObject);
+    }
+
+    private Object unmarshalObject(String type, String value) {
+        Object returnObject = null;
+        switch (type) {
+            case "byte":
+                returnObject = Byte.valueOf(value).byteValue();
+                break;
+            case "short":
+                returnObject = Short.valueOf(value).shortValue();
+                break;
+            case "int":
+                returnObject = Integer.valueOf(value).intValue();
+                break;
+            case "long":
+                returnObject = Long.valueOf(value).longValue();
+                break;
+            case "float":
+                returnObject = Float.valueOf(value).floatValue();
+                break;
+            case "double":
+                returnObject = Double.valueOf(value).doubleValue();
+                break;
+            case "char":
+                returnObject = value.charAt(0);
+                break;
+            case "string":
+                returnObject = value;
+                break;
+            case "boolean":
+                returnObject = Boolean.valueOf(value).booleanValue();
+                break;
+        }
+        return returnObject;
+    }
+
+    private Object[] unmarshalArray(String contentType, JSONArray ary) {
+        Object[] resultAry = new Object[ary.length()];
+        if (contentType.contains("[]")) {
+            String nextDepthType = contentType.replaceFirst("\\[]", "");
+            for (int i = 0; i < ary.length(); i++) {
+                resultAry[i] = unmarshalArray(nextDepthType, ary.getJSONArray(i));
+            }
+        } else {
+            for (int i = 0; i < ary.length(); i++) {
+                resultAry[i] = unmarshalObject(contentType, ary.getString(i));
+            }
+        }
+        return resultAry;
     }
 }
