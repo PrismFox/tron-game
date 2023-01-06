d20928f189c1a9df28239759f61f1f09744d8f00
acx593 <dominik.martin@haw-hamburg.de>
2023-01-06 20:07:01 +0100

    sequenzdiagramme

--- src/main/java/de/haw/vsp/tron/controller/scenechanger/ISceneChanger.java
+++ src/main/java/de/haw/vsp/tron/controller/scenechanger/ISceneChanger.java
@@ -1,7 +1,7 @@
 package de.haw.vsp.tron.controller.scenechanger;
 
 public interface ISceneChanger {
-    public boolean changeToNextScene();
-    public boolean commitAndChangeToNextScene(int startPlayerCounter);
-    public boolean changeToPreviousScene();
+    public void changeToNextScene();
+    public void commitAndChangeToNextScene(int startPlayerCounter);
+    public void changeToPreviousScene();
 }
