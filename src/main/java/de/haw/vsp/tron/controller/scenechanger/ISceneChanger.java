d174722fe850d27b2bea533c9e3c3bc01f3d2aad
Dominik M <muellerxdominik@gmail.com>
2023-01-06 12:56:32 +0100

    Add ImplManager

--- src/main/java/de/haw/vsp/tron/controller/scenechanger/ISceneChanger.java
+++ src/main/java/de/haw/vsp/tron/controller/scenechanger/ISceneChanger.java
@@ -1,8 +1,5 @@
 package de.haw.vsp.tron.controller.scenechanger;
 
-import de.haw.vsp.tron.middleware.annotation.RemoteImplementation;
-
-@RemoteImplementation
 public interface ISceneChanger {
     public boolean changeToNextScene();
     public boolean commitAndChangeToNextScene(int startPlayerCounter);
