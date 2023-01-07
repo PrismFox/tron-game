9808a28c316d500b0fab5a4f25d8e733ef6298bd
Dominik M <muellerxdominik@gmail.com>
2023-01-07 13:50:58 +0100

    Add isPrefixedArg and isAsyncMethod to IImplCaller

--- src/main/java/de/haw/vsp/tron/middleware/applicationstub/ImplManager.java
+++ src/main/java/de/haw/vsp/tron/middleware/applicationstub/ImplManager.java
@@ -12,17 +12,33 @@ import org.springframework.stereotype.Component;
 public class ImplManager implements IImplRegistry, IImplCaller {
 
     private Map<String, Function<Object[], Object>> referenceMap = new HashMap<>();
+    private Map<String, boolean[]> paramPrefixMap = new HashMap<>();
+    private Map<String, Boolean> asyncMethodMap = new HashMap<>();
 
     @Override
     public void registerSyncImplementation(String methodId, Function<Object[], Object> methodReference, boolean[] prefixedArgs) {
         referenceMap.put(methodId, methodReference);
+        paramPrefixMap.put(methodId, prefixedArgs);
+        asyncMethodMap.put(methodId, false);
         //TODO: in ServerStub registrieren
     }
 
     @Override
     public void registerAsyncImplementation(String methodId, Function<Object[], Object> methodReference, boolean[] prefixedArgs) {
         referenceMap.put(methodId, methodReference);
+        paramPrefixMap.put(methodId, prefixedArgs);
+        asyncMethodMap.put(methodId, true);
         //TODO: in ServerStub registrieren
+    }
+
+    @Override
+    public boolean isPrefixedArg(String methodId, int index) {
+        return paramPrefixMap.get(methodId)[index];
+    }
+
+    @Override
+    public boolean isAsyncMethod(String methodId) {
+        return asyncMethodMap.get(methodId);
     }
 
     @Override
