3fce47f39ea7a808b120806b728d1b28dad14671
acu704 <can.heintze@haw-hamburg.de>
2023-01-12 19:41:08 +0100

    [ADD] pom and main for model-controller

---
+++ src/main/java/de/haw/vsp/tron/startGame/StartGameModelController.java
@@ -0,0 +1,15 @@
+package de.haw.vsp.tron.startGame;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("de.haw.vsp.tron")
+public class StartGameModelController {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        ApplicationContext context = new AnnotationConfigApplicationContext(TronConfig.class);
+
+    }
+}
