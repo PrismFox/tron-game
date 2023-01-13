8244cb34d0feefb97d9bdaaf2cf9798b2a8c6696
acu704 <can.heintze@haw-hamburg.de>
2023-01-13 17:48:15 +0100

    [ADD] start class for nameserver

---
+++ src/main/java/de/haw/vsp/tron/startGame/StartNameServer.java
@@ -0,0 +1,13 @@
+package de.haw.vsp.tron.startGame;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("de.haw.vsp.tron")
+public class StartNameServer {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(TronConfig.class);
+    }
+}
