9e4124167b9f79d0b5f51009da951b4891643373
Dominik M <muellerxdominik@gmail.com>
2023-01-13 19:03:47 +0100

    Merge branch 'main' of github.com:PrismFox/tron-game into main

---
+++ package_controller_model.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH="$PATH:"
+
+sed -i "s/@RemoteImplementation/@RemoteInterface/g" src/main/java/de/haw/vsp/tron/view/screens/IScreenHandler.java
+sed -i "s/@RemoteInterface/@RemoteImplementation/g" src/main/java/de/haw/vsp/tron/controller/scenechanger/ISceneChanger.java
+sed -i "s/@RemoteInterface/@RemoteImplementation/g" src/main/java/de/haw/vsp/tron/controller/playercontrol/IPlayerInputManager.java
+
+mvn -f pom-model-controller.xml clean package
+mv target/model-controller-0.1-jar-with-dependencies.jar ./
