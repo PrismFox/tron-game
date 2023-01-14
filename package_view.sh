2b704bb58b49631505721ed471721fb3ad3c2d00
acu704 <can.heintze@haw-hamburg.de>
2023-01-14 11:48:24 +0100

    [FIX] mac scripts

--- package_view.sh
+++ package_view.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "s/@RemoteInterface/@RemoteImplementation/g" src/main/java/de/haw/vsp/tron/view/screens/IScreenHandler.java
-sed -i "s/@RemoteImplementation/@RemoteInterface/g" src/main/java/de/haw/vsp/tron/controller/scenechanger/ISceneChanger.java
-sed -i "s/@RemoteImplementation/@RemoteInterface/g" src/main/java/de/haw/vsp/tron/controller/playercontrol/IPlayerInputManager.java
+sed -i "" "s/@RemoteInterface/@RemoteImplementation/g" src/main/java/de/haw/vsp/tron/view/screens/IScreenHandler.java
+sed -i "" "s/@RemoteImplementation/@RemoteInterface/g" src/main/java/de/haw/vsp/tron/controller/scenechanger/ISceneChanger.java
+sed -i "" "s/@RemoteImplementation/@RemoteInterface/g" src/main/java/de/haw/vsp/tron/controller/playercontrol/IPlayerInputManager.java
 
 mvn -f pom-view.xml clean package
 mv target/view-library-0.1-jar-with-dependencies.jar ./
