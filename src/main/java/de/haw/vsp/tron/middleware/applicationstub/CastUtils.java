1867b97f759235ec8b32fceaa648106b28daea5d
Dominik M <muellerxdominik@gmail.com>
2023-01-15 01:22:22 +0100

    Potentially fix CastUtils

--- src/main/java/de/haw/vsp/tron/middleware/applicationstub/CastUtils.java
+++ src/main/java/de/haw/vsp/tron/middleware/applicationstub/CastUtils.java
@@ -13,13 +13,13 @@ public class CastUtils {
     }
 
     public static <T> T deepCast(Object[] input, Class<T> clazz) { 
-        T result = (T) Array.newInstance(clazz, Array.getLength(input));
-        for (int i = 0; i < Array.getLength(input); i++) {
-            Object element = Array.get(input, i);
+        T result = (T) Array.newInstance(clazz.getComponentType(), Array.getLength(input)); // [[25, 32]], Integer[][] // [25, 32], Integer[]
+        for (int i = 0; i < Array.getLength(input); i++) { // 
+            Object element = Array.get(input, i);           //[25, 32], Integer[][]
             if (element.getClass().isArray()) {
-                Array.set(result, i, deepCast((Object[]) element, clazz.getComponentType()));
+                Array.set(result, i, deepCast((Object[]) element, clazz.getComponentType()));   //[25, 32], Integer[]
             } else {
-                Array.set(result, i, clazz.getComponentType().cast(element));
+                Array.set(result, i, clazz.getComponentType().cast(element)); // Integer
             }
         }
         return result;
